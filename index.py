73db8374d6c494145eae2278d1879dd96338d080
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-03-06 13:23:21 -0700

    Added Sources to index file

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
+###############################################################################
+# Main Project File for Machine Learning Class Project
+# Project: Learning Language Model - Building a Book
+# Class: Machine Learning and Data Mining - CS450 - Winter 2020
+# Contributors:
+# - <name>
+# - <name>
+# - <name>
+# - <name>
+# Sources:
+# - https://openai.com/blog/better-language-models/
+# - https://github.com/openai/gpt-2
+# - https://cdn.openai.com/better-language-models/language_models_are_unsupervised_multitask_learners.pdf
+###############################################################################
+def main():
+   pass
 
+if __name__ == "__main__":
+   main()
