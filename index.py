5f813073db37d755a3ebfffa2922fd822ad7fa13
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-03-07 11:22:28 -0700

    Added source

--- index.py
+++ index.py
@@ -8,6 +8,11 @@
 # - <name>
 # - <name>
 # Sources:
+# @article{radford2019language,
+#   title={Language Models are Unsupervised Multitask Learners},
+#   author={Radford, Alec and Wu, Jeff and Child, Rewon and Luan, David and Amodei, Dario and Sutskever, Ilya},
+#   year={2019}
+# }
 # - https://openai.com/blog/better-language-models/
 # - https://github.com/openai/gpt-2
 # - https://cdn.openai.com/better-language-models/language_models_are_unsupervised_multitask_learners.pdf
