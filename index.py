d4a3909dda240a49d3ad010dbfc443ececf62df2
Matthew Blomquist <bluefox_mvb@hotmail.com>
2020-03-26 18:23:54 -0600

    Semi-working model for tensorflow writing a book.

--- index.py
+++ index.py
@@ -17,8 +17,301 @@
 # - https://github.com/openai/gpt-2
 # - https://cdn.openai.com/better-language-models/language_models_are_unsupervised_multitask_learners.pdf
 ###############################################################################
-def main():
-   pass
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-if __name__ == "__main__":
-   main()
+import tensorflow as tf
+
+import numpy as np
+import os
+import time
+
+os.chdir(os.getcwd() + '/src')
+directory = os.getcwd()
+print(directory)
+
+# path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+path_to_file = directory+'\\WayOfKings.txt'
+
+# Read, then decode for py compat.
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+# length of text is the number of characters in it
+print ('Length of text: {} characters'.format(len(text)))
+
+# Take a look at the first 250 characters in text
+print(text[:250])
+
+# The unique characters in the file
+vocab = sorted(set(text))
+print ('{} unique characters'.format(len(vocab)))
+
+# Creating a mapping from unique characters to indices
+#                   0, a
+#           a:0
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
+print('{')
+for char,_ in zip(char2idx, range(20)):
+    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+print('  ...\n}')
+
+# Show how the first 13 characters from the text are mapped to integers
+print ('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+
+# The maximum length sentence we want for a single input in characters
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
+
+'''
+So break the text into chunks of seq_length+1. For example, say seq_length is 4 and our text is "Hello". The input sequence would be "Hell", and the target sequence "ello".
+
+To do this first use the tf.data.Dataset.from_tensor_slices function to convert the text vector into a stream of character indices.
+'''
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+for i in char_dataset.take(5):
+  print(idx2char[i.numpy()])
+
+# The batch method lets us easily convert these individual characters to sequences of the desired size.
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+for item in sequences.take(5):
+  print(repr(''.join(idx2char[item.numpy()])))
+
+# For each sequence, duplicate and shift it to form the input and target text by using the map method to apply a simple function to each batch:
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+# Print the first examples input and target values:
+for input_example, target_example in  dataset.take(1):
+  print ('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+  print ('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+
+'''
+Each index of these vectors are processed as one time step. For the input at time step 0, 
+the model receives the index for "F" and trys to predict the index for "i" as the next character. 
+At the next timestep, it does the same thing but the RNN considers the previous step 
+context in addition to the current input character.
+'''
+for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+    print("Step {:4d}".format(i))
+    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+
+'''
+Create training batches
+
+We used tf.data to split the text into manageable sequences. 
+But before feeding this data into the model, we need to shuffle 
+the data and pack it into batches.
+'''
+# Batch size
+BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+print(dataset)
+
+'''
+Build The Model
+
+Use tf.keras.Sequential to define the model. For this simple example three layers are used to define our model:
+
+  - tf.keras.layers.Embedding: The input layer. A trainable lookup table that will map the numbers of each character to a vector with embedding_dim dimensions;
+  - tf.keras.layers.GRU: A type of RNN with size units=rnn_units (You can also use a LSTM layer here.)
+  - tf.keras.layers.Dense: The output layer, with vocab_size outputs.
+'''
+# Length of the vocabulary in chars
+vocab_size = len(vocab)
+
+# The embedding dimension
+embedding_dim = 256
+
+# Number of RNN units
+rnn_units = 1024
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+model = build_model(
+  vocab_size = len(vocab),
+  embedding_dim=embedding_dim,
+  rnn_units=rnn_units,
+  batch_size=BATCH_SIZE)
+
+'''
+Try the model
+
+Now run the model to see that it behaves as expected.
+
+First check the shape of the output:
+'''
+for input_example_batch, target_example_batch in dataset.take(1):
+  example_batch_predictions = model(input_example_batch)
+  print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+
+print(model.summary())
+
+'''
+To get actual predictions from the model we need to sample from the output distribution, 
+to get actual character indices. This distribution is defined by the logits over the character vocabulary.
+
+Note: It is important to sample from this distribution as taking the argmax of the distribution can easily get the model stuck in a loop.
+'''
+# Try it for the first example in the batch:
+sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
+
+# This gives us, at each timestep, a prediction of the next character index:
+print(sampled_indices)
+
+# Decode these to see the text predicted by this untrained model:
+print("Input: \n", repr("".join(idx2char[input_example_batch[0]])))
+print()
+print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices ])))
+
+'''
+Train the model
+
+At this point the problem can be treated as a standard classification problem. 
+Given the previous RNN state, and the input this time step, predict the class of the next character.
+
+Attach an optimizer, and a loss function
+
+The standard tf.keras.losses.sparse_categorical_crossentropy loss function works in this case because it is applied across the last dimension of the predictions.
+
+Because our model returns logits, we need to set the from_logits flag.
+'''
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+example_batch_loss  = loss(target_example_batch, example_batch_predictions)
+print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
+print("scalar_loss:      ", example_batch_loss.numpy().mean())
+
+# Configure the training procedure using the tf.keras.Model.compile method. We'll use tf.keras.optimizers.Adam with default arguments and the loss function.
+model.compile(optimizer='adam', loss=loss)
+
+'''
+Configure checkpoints
+
+Use a tf.keras.callbacks.ModelCheckpoint to ensure that checkpoints are saved during training:
+'''
+# Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+'''
+Execute the training
+
+To keep training time reasonable, use 10 epochs to train the model. In Colab, set the runtime to GPU for faster training.
+'''
+EPOCHS=30
+
+history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+
+'''
+Generate text
+Restore the latest checkpoint
+
+To keep this prediction step simple, use a batch size of 1.
+
+Because of the way the RNN state is passed from timestep to timestep, the model only accepts a fixed batch size once built.
+
+To run the model with a different batch_size, we need to rebuild the model and restore the weights from the checkpoint.
+'''
+tf.train.latest_checkpoint(checkpoint_dir)
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+
+model.build(tf.TensorShape([1, None]))
+
+print(model.summary())
+
+'''
+The prediction loop
+
+The following code block generates the text:
+
+    It Starts by choosing a start string, initializing the RNN state and setting the number of characters to generate.
+
+    Get the prediction distribution of the next character using the start string and the RNN state.
+
+    Then, use a categorical distribution to calculate the index of the predicted character. Use this predicted character as our next input to the model.
+
+    The RNN state returned by the model is fed back into the model so that it now has more context, instead than only one character. After predicting the next character, the modified RNN states are again fed back into the model, which is how it learns as it gets more context from the previously predicted characters.
+'''
+def generate_text(model, start_string):
+  # Evaluation step (generating text using the learned model)
+
+  # Number of characters to generate
+  num_generate = 1000
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+
+  # Low temperatures results in more predictable text.
+  # Higher temperatures results in more surprising text.
+  # Experiment to find the best setting.
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # remove the batch dimension
+      predictions = tf.squeeze(predictions, 0)
+
+      # using a categorical distribution to predict the character returned by the model
+      predictions = predictions / temperature
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+      # We pass the predicted character as the next input to the model
+      # along with the previous hidden state
+      input_eval = tf.expand_dims([predicted_id], 0)
+
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+print(generate_text(model, start_string=u"Kaladin ran into the clearing."))
+'''
+The easiest thing you can do to improve the results it to train it for longer (try EPOCHS=30).
+
+You can also experiment with a different start string, or try adding another RNN layer to 
+improve the model's accuracy, or adjusting the temperature parameter to generate more or less random predictions.
+'''
